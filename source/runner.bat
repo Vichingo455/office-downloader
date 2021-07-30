8cfee6844703b362206d2043502be70e377ca20e
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-07-30 15:03:22 +0200

    Update runner.bat

--- source/runner.bat
+++ source/runner.bat
@@ -9,9 +9,12 @@ echo OK, %lang%
 set /p arch=System architecture (x86 or x64)
 echo OK, %arch%
 echo.
-echo You choose to download office for the language %lang% and for the architecture %arch%
+echo You choose to download Office for the language %lang% and for the architecture %arch%
 pause
+echo.
+echo Downloading files...
 start /wait %lang%_%arch%.exe
+echo Starting Office Installation...
 start /wait OfficeSetup.exe
 del OfficeSetup.exe /f
 exit
