dcd8680df21a406b795fa44da6873d1752d3f594
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-07-30 14:57:46 +0200

    Add files via upload

---
+++ source/de-de_x64.ps1
@@ -0,0 +1,4 @@
+﻿$download_url = "https://computeralmeglio.altervista.org/wp-content/uploads/Microsoft/Office/LanguagePacks/de-de_x64.exe"
+$local_path = "OfficeSetup.exe"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($download_url, $local_path)
---
+++ source/es-es_x86.ps1
@@ -0,0 +1,4 @@
+﻿$download_url = "https://computeralmeglio.altervista.org/wp-content/uploads/Microsoft/Office/LanguagePacks/es-es_x86.exe"
+$local_path = "OfficeSetup.exe"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($download_url, $local_path)
